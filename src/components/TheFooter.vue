1bcb9fd1d52ef6bde608772d7d3f862a6987841f
br4nc4 <brancaleon.giuseppe@gmail.com>
2022-06-21 11:39:25 +0200

    header/footer

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <footer>
+            <div class="container">
+
+            </div>
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TheFooter",
+}
+</script>
+
+<style>
+    footer{
+        background-image: url("/public/img/footer-montagne-bottom.jpg");
+        background-size: contain;
+        background-position: center bottom;
+        background-repeat: no-repeat;
+        padding-top: 2rem;
+        padding-bottom: 8rem;
+    }
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <img src="/img/la-molisana-logo.png" alt="">
+
+        <ul class="nav justify-content-center">
+            <li class="nav-item" v-for="(link, i) in navLinks" :key="i">
+                <a class="nav-link" aria-current="page" :href="link.href">{{link.name}}</a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TheHeader",
+    data(){
+        return{
+            navLinks: [
+                {
+                    name: "Home",
+                    href: "#",
+                },
+                {
+                    name: "Prodotti",
+                    href: "#",
+                },
+                {
+                    name: "Chi Siamo",
+                    href: "#",
+                },
+                {
+                    name: "Contatti",
+                    href: "#",
+                },
+                ]
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
